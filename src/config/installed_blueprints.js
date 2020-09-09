5795c1aed64694801e29c573304358b51ef4ec29
Crowdbotics <team@crowdbotics.com>
2020-09-09 06:47:50 +0000

    add package blueprint-react-native-feed-screen

--- src/config/installed_blueprints.js
+++ src/config/installed_blueprints.js
@@ -1,5 +1,6 @@
 export const installed_blueprints = [
   //@BlueprintInsertion
+{ name: 'Feed2099242', human_name: 'Feed 20', access_route: 'Feed2099242'},
 { name: 'Dashboard12499236', human_name: 'Dashboard 1 24', access_route: 'Dashboard12499236'},
 { name: 'Settings99217', human_name: 'Settings', access_route: 'Settings99217'},
 
